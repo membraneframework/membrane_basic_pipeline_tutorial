a52f90da98e30635f45926df48be599b7127d4fc
varsill <lukasz.kita0@gmail.com>
2022-02-02 15:19:20 +0100

    Add Mixer test. Add depayloader tests made with Membrane.Testing

--- test/elements/depayloader_test.exs
+++ test/elements/depayloader_test.exs
@@ -3,15 +3,77 @@ defmodule DepayloaderTest do
   alias Basic.Elements.Depayloader
   alias Membrane.Buffer
 
-  @initial_state %{
-    frame: [],
-    packets_per_frame: 5
-  }
+  alias Membrane.Testing.{Source, Sink}
+
+  import Membrane.Testing.Assertions
+  alias Membrane.Testing.{Source, Sink, Pipeline}
+
 
   doctest Basic.Elements.OrderingBuffer
 
+  test "Depayloader should assemble the packets and form a frame (with membrane's testing framework)" do
+    inputs = ["[frameid:1s][timestamp:1]Hello! ", "[frameid:1][timestamp:1]How are", "[frameid:1e][timestamp:1] you?"]
+    options = %Pipeline.Options{
+      elements: [
+        source: %Source{output: inputs, caps: %Basic.Formats.Packet{type: :custom_packets}},
+        depayloader: %Depayloader{packets_per_frame: 5},
+        sink: Sink
+      ]
+    }
+
+    {:ok, pipeline} = Pipeline.start_link(options)
+    Pipeline.play(pipeline)
+    assert_start_of_stream(pipeline, :sink)
+
+    assert_sink_buffer(pipeline,:sink, %Buffer{payload: "Hello! How are you?"})
+
+    assert_end_of_stream(pipeline, :sink)
+    refute_sink_buffer(pipeline, :sink, _, 0)
+    Pipeline.stop_and_terminate(pipeline, blocking?: true)
+  end
+
+
+  test "Depayloader should assemble the packets and form a frame (with membrane's testing framework based on generator)" do
+    initial_state = ["[frameid:1s][timestamp:1]Hello! ", "[frameid:1][timestamp:1]How are", "[frameid:1e][timestamp:1] you?"]
+    generator = fn state, size ->
+      if state == [] do
+        {[end_of_stream: :output], state}
+      else
+        [payload| new_state] = state
+        if size > 1 do
+          {[buffer: {:output, %Buffer{payload: payload}}, redemand: :output], new_state}
+        else
+          {[buffer: {:output, %Buffer{payload: payload}}], new_state}
+        end
+      end
+    end
+
+    options = %Pipeline.Options{
+      elements: [
+        source: %Source{output: {initial_state, generator}, caps: %Basic.Formats.Packet{type: :custom_packets}},
+        depayloader: %Depayloader{packets_per_frame: 5},
+        sink: Sink
+      ]
+    }
+
+    {:ok, pipeline} = Pipeline.start_link(options)
+    Pipeline.play(pipeline)
+    assert_start_of_stream(pipeline, :sink)
+
+    assert_sink_buffer(pipeline,:sink, %Buffer{payload: "Hello! How are you?"})
+
+    assert_end_of_stream(pipeline, :sink)
+    refute_sink_buffer(pipeline, :sink, _, 0)
+    Pipeline.stop_and_terminate(pipeline, blocking?: true)
+  end
+
+
+
+
+
   test "Depayloader should assemble the packets and form a frame" do
-    {:ok, state} = Depayloader.handle_process(:input, %Buffer{payload: "[frameid:1s][timestamp:1]Hello! "}, nil, @initial_state)
+    {:ok, state} = Depayloader.handle_init(%Depayloader{packets_per_frame: 5})
+    {:ok, state} = Depayloader.handle_process(:input, %Buffer{payload: "[frameid:1s][timestamp:1]Hello! "}, nil, state)
     {:ok, state} = Depayloader.handle_process(:input, %Buffer{payload: "[frameid:1][timestamp:1]How are"}, nil, state)
     {{:ok, actions}, _state} = Depayloader.handle_process(:input, %Buffer{payload: "[frameid:1e][timestamp:1] you?"}, nil, state)
     [buffer: {:output, buffer}] = actions
---
+++ test/elements/mixer_test.exs
@@ -0,0 +1,65 @@
+defmodule MixerTest do
+  use ExUnit.Case
+  alias Basic.Elements.Mixer
+  alias Membrane.Buffer
+
+  import Membrane.Testing.Assertions
+  alias Membrane.Testing.{Source, Sink, Pipeline}
+  import Membrane.ParentSpec
+
+  doctest Basic.Elements.Mixer
+
+  @first_input_frames [
+    %Buffer{payload: "TEST FRAME", pts: 2},
+    %Buffer{payload: "TEST FRAME", pts: 5}
+  ]
+
+  @second_input_frames [
+    %Buffer{payload: "TEST FRAME", pts: 1},
+    %Buffer{payload: "TEST FRAME", pts: 3},
+    %Buffer{payload: "TEST FRAME", pts: 4}
+  ]
+
+  test "Mixer should mix frames coming from two sources, based on the timestamps" do
+
+    generator = fn state, size ->
+      if state == [] do
+        {[end_of_stream: :output], state}
+      else
+        [buffer| new_state] = state
+        if size > 1 do
+          {[buffer: {:output, buffer}, redemand: :output], new_state}
+        else
+          {[buffer: {:output, buffer}], new_state}
+        end
+      end
+    end
+
+    options = %Pipeline.Options{
+      elements: [
+        source1: %Source{output: {@first_input_frames, generator}, caps: %Basic.Formats.Frame{encoding: :utf8}},
+        source2: %Source{output: {@second_input_frames, generator}, caps: %Basic.Formats.Frame{encoding: :utf8}},
+        mixer: Mixer,
+        sink: Sink
+      ],
+      links: [
+        link(:source1) |> via_in(:first_input) |> to(:mixer),
+        link(:source2) |> via_in(:second_input) |> to(:mixer),
+        link(:mixer) |> to(:sink)
+      ]
+    }
+    {:ok, pipeline} = Pipeline.start_link(options)
+
+    assert_start_of_stream(pipeline, :sink)
+    assert_
+    assert_sink_buffer(pipeline,:sink, %Buffer{pts: 1})
+    assert_sink_buffer(pipeline,:sink, %Buffer{pts: 2})
+    assert_sink_buffer(pipeline,:sink, %Buffer{pts: 3})
+    assert_sink_buffer(pipeline,:sink, %Buffer{pts: 4})
+    assert_sink_buffer(pipeline,:sink, %Buffer{pts: 5})
+    assert_end_of_stream(pipeline, :sink)
+    refute_sink_buffer(pipeline, :sink, _, 0)
+    Pipeline.stop_and_terminate(pipeline, blocking?: true)
+  end
+
+end
