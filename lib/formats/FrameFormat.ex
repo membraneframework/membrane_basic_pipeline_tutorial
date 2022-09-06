abbd5f98c5573c16f0e870202a8b69a33148dcbc
varsill <lukasz.kita0@gmail.com>
2022-09-06 16:02:40 +0200

    Remove the code template so that the master branch contains only README and LICENSE

--- lib/formats/FrameFormat.ex
+++
@@ -1,6 +0,0 @@
-defmodule Basic.Formats.Frame do
-  @moduledoc """
-  A module describing the format of the frame.
-  """
-  defstruct encoding: :utf8
-end
--- lib/start.exs
+++
@@ -1,3 +0,0 @@
-{:ok, pid} = Basic.Pipeline.start()
-Basic.Pipeline.play(pid)
-Process.sleep(500)
